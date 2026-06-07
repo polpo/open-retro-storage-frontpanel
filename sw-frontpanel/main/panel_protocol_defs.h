7104e12ecc971ff9006d50c6c92900ff274841e2
Eric Helgeson <erichelgeson@gmail.com>
2026-06-07 18:31:40 -0500

    Relicense panel_protocol_defs.h to GPL-2.0-or-later
    Allows this shared protocol header to be included in GPLv3+ projects
    (e.g. BlueSCSI). Only this file is relicensed; the rest of the project
    remains GPL-2.0-only. Done with the consent of all copyright holders.

--- sw-frontpanel/main/panel_protocol_defs.h
+++ sw-frontpanel/main/panel_protocol_defs.h
@@ -1,10 +1,14 @@
-// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-License-Identifier: GPL-2.0-or-later
 //
-//  Copyright (C) 2025  Ian Scott
+//  Copyright (C) 2025-2026  Ian Scott
+//  Copyright (C) 2026       Eric Helgeson
+//
+//  NOTE: This file alone is licensed GPL-2.0-or-later.
 //
 //  This program is free software; you can redistribute it and/or modify it
-//  under the terms of the GNU General Public License (as published by the
-//  Free Software Foundation) version 2, dated June 1991.
+//  under the terms of the GNU General Public License as published by the
+//  Free Software Foundation; either version 2 of the License, or (at your
+//  option) any later version.
 //
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
